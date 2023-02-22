2aa88017d5d7648dbacec476bcc9a09fca19bd75
Sandra Sáez Ibarra <sansaeiba@alu.edu.gva.es>
2023-02-22 23:45:39 +0100

    Versión final con la documantación JavaDoc

--- EntradaBlogSSI.java
+++ EntradaBlogSSI.java
@@ -1,8 +1,10 @@
 package EntradaBlog;
 
-/*Clase para manejar las entradas de un blog.
- * La clase se creó el 21/3/1999 por Ana López.
- * Esta es la versión 2.3
+/**
+ * Clase para manejar las entradas de un blog.
+ * La clase se creó el 22/2/2023.
+ * @author Sandra Sáez Ibarra
+ * @version 2.3
  */
 
 public class EntradaBlogSSI {
@@ -15,16 +17,26 @@ public class EntradaBlogSSI {
         private String texto;
         private String autor;
 
-        //Constructor de la clase. recibe el número de entrada, el nombre del autor
-        //de la entrada y el texto que contiene la entrada. Si el número de entrada
-        //es negativo, lanza una excepción.
+
+    /**
+     * Este método lanza una excepción
+     * @param id int recibe número
+     * @param autor String recibe el nombre del autor
+     * @param texto String recibe el texto que contiene la entrada
+     * @throws IllegalArgumentException
+     */
 	public EntradaBlogSSI (int id,String autor,String texto)throws IllegalArgumentException{
             if(id<=0) throw new IllegalArgumentException("El id no puede ser negativo");
             this.id=id;
             this.autor=autor;
             this.texto=texto;
         }
-        @Override
+
+    /**
+     *
+     * @return toString String devuelve una cadena de texto
+     */
+    @Override
         public String toString(){
             String cad="";
             cad+="\nENTRADA DE"+separador+autor;
@@ -32,30 +44,50 @@ public class EntradaBlogSSI {
             return cad;
         }
 
-        //Devuelve el número de la entrada
+    /**
+     * Este método devuelve el número de la entrada
+     * @return getId int
+     */
+
         public int getId(){
             return this.id;
         }
 
-        //devuelve el texto completo de la entrada
+    /**
+     * Este método devuelve el texto completo de la entrada
+     * @return getTexto String
+     */
+
         public String getTexto(){
             return this.texto;
         }
 
-        //devuelve el nombre del autor de la entrada en mayúsculas
+    /**
+     * Este método devuelve el nombre del autor de la entrada en mayúsculas
+     * @return getAutor String
+     */
+
         public String getAutor(){
             return this.autor.toUpperCase();
         }
 
-        //devuelve el nombre del autor. Ya no se usa. Mejor ver getAutor
+    /**
+     * Este método devuelve el nombre del autor. Ya no se usa. Mejor ver getAutor
+     * @return devuelveAutor String
+     */
+
         public String devuelveAutor(){
             return this.autor;
         }
 
-        //No tiene porqué tener argumentos.
+    /**
+     * No tiene porqué tener argumentos
+     * @param args
+     */
+
         public static void main(String[] args) {
             //Modificar.
-            EntradaBlogSSI e1=new EntradaBlogSSI (-3,"ana","Últimas noticias, está disponible BixBy 2.0");
+            EntradaBlogSSI e1=new EntradaBlogSSI (-3,"sandra","Últimas noticias, está disponible BixBy 2.0");
             System.out.println(e1);
         }
 
